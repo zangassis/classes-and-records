fccfbb72f61e06e6d99003b027f69abd70cc670a
assiszang <assis.zang@animaeducacao.com.br>
2024-06-19 15:32:27 -0300

    refactor: Update program class

--- Program.cs
+++ Program.cs
@@ -24,7 +24,6 @@ var productNew = new Product()
 
 Product newProduct = new Product(1, "Laptop", "High-performance laptop", 2500.00m, 10);
 
-
 var productDto = new ProductDto("Laptop", 1500.00m, "Electronics");
 
 var productDtoModified = productDto with { Category = "Laptops" };
@@ -34,18 +33,8 @@ Address address = new Address("123 Main St", "Springfield", "IL", "62701");
 var (street, city, state, zipCode) = address;
 Console.WriteLine($"Street: {street}, City: {city}, State: {state}, ZipCode: {zipCode}");
 
-
-
-
-
 var customer = new Customer("John Smith", "john@example.com", "95940033");
 
 var internalCustomer = new InternalCustomer(10, "John Smith", "john@example.com", "95940033");
-
-
-
-
-
-app.MapGet("/", () => "Hello World!");
 
 app.Run();
